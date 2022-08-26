8de1babc532c7c39d315b7419f85dab1361b3c33
Kirill Miller <belogorski2000@gmail.com>
2022-08-26 10:15:17 +0300

    Добавьте файлы проекта.

---
+++ _14/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://akusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Снежооок", "МяяяяяяяЯЯУУУ!!!");
+            cat.ShowInfo();
+            Dog dog = new Dog("Гавгавыч", "ААААААввввВ!?!?!");
+            dog.ShowInfo();
+            Console.WriteLine();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void GetSay();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            GetSay();
+        }
+    }
+    class Cat : Animal
+    {
+        string name;
+        string say;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name, string say)
+       : base(name)
+        {
+            this.say = say;
+        }
+        public override void GetSay()
+        {
+            Console.WriteLine("МяяяяяяяЯЯУУУ!!!");
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        string say;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string say)
+       : base(name)
+        {
+            this.say = say;
+        }
+        public override void GetSay()
+        {
+            Console.WriteLine("ААААААввввВ!?!?!");
+        }
+    }
+}
